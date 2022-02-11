f3515c8d4c7f06a9678ad420379f91d4ff91054e
Manou-02 <mrakotoarivelo97@gmail.com>
2022-02-11 15:14:08 +0100

    Quelque modif

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h4>Home page</h4>
+      <h4> Home page</h4>
+      <p>Welcome to my first next deploy page</p>
     </div>
   )
 }
